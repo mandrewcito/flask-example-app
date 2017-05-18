b1ebc80ac531c1ecb2554c968ae5a5ed2146ef65
mandrewcito <anbaalo@gmail.com>
2017-05-19 00:10:01 +0200

    quitar fichero

--- app/module1/2ontrollers.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Blueprint
-
-module2 = Blueprint('module2',__name__,url_prefix='/module2')
-
-@module2.route('/hello',methods=['GET','POST'])
-def hello():
-    return "[2] Hello, world!"
-
