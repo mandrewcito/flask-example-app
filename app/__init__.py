92af2328a1b1c9a137999a1d8d080c05f0eaf9d1
mandrewcito <anbaalo@gmail.com>
2017-05-24 00:06:36 +0200

    Blueprint as a class and register methods as endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from app.module1.controllers import module1 as m1
+from app.module1.controllers import MyModule as m1
 from app.module2.controllers import module2 as m2
 from app.parameter.controllers import parameter_module as parameter_module
 
@@ -9,6 +9,6 @@ app = Flask(__name__)
 #app.config.from_object('config')
 
 #Register modules
-app.register_blueprint(m1)
+app.register_blueprint(m1())
 app.register_blueprint(m2)
 app.register_blueprint(parameter_module)
