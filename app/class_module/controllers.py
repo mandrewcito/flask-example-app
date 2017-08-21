ca9efe7c43ed179ef9961d3c65701c9955b85072
mandrewcito <anbaalo@gmail.com>
2017-08-21 20:22:30 +0200

    borrar organizacion vieja

--- app/class_module/controllers.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-class ClassModule(Blueprint):
-    def __init__(self):
-        super(ClassModule,self).__init__("class_module",__name__,url_prefix='/class_module')
-        self.add_url_rule("/hello",view_func=self.hello,methods=['GET'])
-
-    def hello(self):
-        return "hello,world"
--- run.py
+++
@@ -1,6 +0,0 @@
-from app.application import ExampleApi
-
-from config import Configuration
-
-cfg = Configuration(debug=True)
-ExampleApi(cfg).run(host=cfg.host,port=cfg.port,debug=cfg.debug)
